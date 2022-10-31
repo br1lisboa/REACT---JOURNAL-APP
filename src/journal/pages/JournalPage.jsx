8e0cf16d5ca5ffe1a0a529c99a2b90a972241b83
Bruno Lisboa <brlisbo@gmail.com>
2022-10-31 18:06:12 -0300

    login page whitout design

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,10 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+/* Se debe usar con el param variante, por defecto es un parrafo */
 
 export const JournalPage = () => {
     return (
         <>
-        /* Se debe usar con el param variante, por defecto es un parrafo */
             <Typography variant="h1">JournalPage</Typography>
             <MailOutline />
         </>
