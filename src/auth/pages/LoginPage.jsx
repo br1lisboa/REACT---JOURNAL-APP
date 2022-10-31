abe22bd41d96efc2438da6e5c3a73c5e4e75db7f
Bruno Lisboa <brlisbo@gmail.com>
2022-10-31 18:20:40 -0300

    login page whitout design

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,15 @@
+import { Grid } from "@mui/material"
 
 export const LoginPage = () => {
     return (
-        <h1>LoginPage</h1>
+        /* Grid se puede ver como un div pero con props interesantes, la mayor parte de los componentes de material vienen con un xs, y tenemos una popiedad sx que es style extended y tenemos acceso al tema que definimos con nuestro teamProvider*/
+        <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}>
+            {/* xs en pantallas pequeñas tendra 3 posiciones, tenemos md, xl */}
+            <Grid item className="box-shadow" xs={3} sx={{backgroundColor}}>
+
+
+            </Grid>
+
+        </Grid>
     )
 }
