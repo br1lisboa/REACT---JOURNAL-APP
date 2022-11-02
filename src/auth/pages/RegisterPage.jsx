3a3e35e65df00772dcf4733bcdd27ea2463a08ff
Bruno Lisboa <brlisbo@gmail.com>
2022-11-02 09:24:26 -0300

    finished registerPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,65 @@
+import { Link as RouterLink } from "react-router-dom";
 
-const RegisterPage = () => {
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from "../layout/AuthLayout";
+
+export const RegisterPage = () => {
     return (
-        <div>RegisterPage</div>
+        /* Grid se puede ver como un div pero con props interesantes, la mayor parte de los componentes de material vienen con un xs, y tenemos una popiedad sx que es style extended y tenemos acceso al tema que definimos con nuestro teamProvider*/
+        <AuthLayout title="Crear cuenta">
+
+            <form>
+                {/* Contenedor del form */}
+                <Grid container >
+                    {/* Input datos */}
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Nombre completo"
+                            type="text"
+                            placeholder="Jhon Doe"
+                            fullWidth />
+                    </Grid>
+                    {/* Input correo */}
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Correo"
+                            type="email"
+                            placeholder="correo@google.com"
+                            fullWidth />
+                    </Grid>
+                    {/* Input contraseña */}
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Contraseña"
+                            type="paswword"
+                            placeholder="Contraseña"
+                            fullWidth />
+                    </Grid>
+
+                    {/* Contenedor botones */}
+                    <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                        {/* Boton login */}
+                        <Grid item xs={12} >
+                            <Button variant="contained" fullWidth>
+                                Crear cuenta
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    {/* Contenedor link a pagina de registro */}
+                    <Grid container
+                        direction="row"
+                        justifyContent="end">
+                        <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+                        {/* Link a register, con RouterLink ya que renombramos en la importacion el componente Link de react */}
+                        <Link component={RouterLink} color="inherit" to="/auth/login">
+                            Ingresar
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+
+        </AuthLayout>
     )
 }
-
-export default RegisterPage
