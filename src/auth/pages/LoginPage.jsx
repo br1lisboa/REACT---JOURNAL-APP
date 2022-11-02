7413f9872e872b6880236afbbfea8a1b72bd2b07
br1lisboa <brlisbo@gmail.com>
2022-11-01 22:02:33 -0300

    working on loginPage styles

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,15 +1,48 @@
-import { Grid } from "@mui/material"
+import { Grid, TextField, Typography } from "@mui/material"
 
 export const LoginPage = () => {
     return (
         /* Grid se puede ver como un div pero con props interesantes, la mayor parte de los componentes de material vienen con un xs, y tenemos una popiedad sx que es style extended y tenemos acceso al tema que definimos con nuestro teamProvider*/
-        <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}>
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}>
+
             {/* xs en pantallas pequeñas tendra 3 posiciones, tenemos md, xl */}
-            <Grid item className="box-shadow" xs={3} sx={{backgroundColor}}>
+            <Grid item
+                className="box-shadow"
+                xs={3}
+                sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
 
+                <Typography variant="h5"
+                    sx={{ mb: 1 }}>
+                    Login
+                </Typography>
 
-            </Grid>
+                <form>
+                    <Grid container >
+                        <Grid item xs={12} sx={{ mt: 2 }}>
+                            <TextField
+                                label="Correo"
+                                type="email"
+                                placeholder="correo@google.com"
+                                fullWidth />
+                        </Grid>
+                        <Grid item xs={12} sx={{ mt: 2 }}>
+                            <TextField
+                                label="Contraseña"
+                                type="paswword"
+                                placeholder="Contraseña"
+                                fullWidth />
+                        </Grid>
+                    </Grid>
+                </form>
 
-        </Grid>
+            </Grid >
+
+        </Grid >
     )
 }
