1fc30ec21899fe4c86c24d27a578546cd8726d7c
br1lisboa <brlisbo@gmail.com>
2022-11-09 21:11:25 -0300

    custom hook useCheckAuth aply ok

---
+++ src/hooks/useCheckAuth.js
@@ -0,0 +1,31 @@
+import { onAuthStateChanged } from "firebase/auth"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { firebaseAuth } from "../firebase/config"
+import { login, logout } from "../store/auth/authSlice"
+
+
+export const useCheckAuth = () => {
+
+    const { status } = useSelector(state => state.auth)
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+
+        /* FIREBASE NOS OFRECE UNA FORMA DE ESTAR PENDIENTE DE LOS CAMBIOS QUE ESE USUARIO VA A TENER */
+        onAuthStateChanged(firebaseAuth, async (user) => {
+            if (!user) return dispatch(logout)
+
+
+            const { uid, email, displayName, photoURL } = user
+            dispatch(login({ uid, email, displayName, photoURL }))
+        }) /* ESTA ES UNA FUNCION QUE EMITE VALORES CUANDO EL ESTADO CAMBIA */
+
+    }, [])
+
+    return {
+        status
+    }
+
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,32 +1,14 @@
-import { onAuthStateChanged } from "firebase/auth"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoute } from "../auth/routes/AuthRoute"
-import { firebaseAuth } from "../firebase/config"
+
+import { useCheckAuth } from "../hooks"
 import { JournalRoute } from "../journal/routes/JournalRoute"
-import { login, logout } from "../store/auth/authSlice"
+
 import { ChekingAuth } from "../ui/components/ChekingAuth"
 
 export const AppRouter = () => {
 
-    const { status } = useSelector(state => state.auth)
-    const dispatch = useDispatch()
-
-
-    useEffect(() => {
-
-        /* FIREBASE NOS OFRECE UNA FORMA DE ESTAR PENDIENTE DE LOS CAMBIOS QUE ESE USUARIO VA A TENER */
-        onAuthStateChanged(firebaseAuth, async (user) => {
-            if (!user) return dispatch(logout)
-
-
-            const { uid, email, displayName, photoURL } = user
-            dispatch(login({ uid, email, displayName, photoURL }))
-        }) /* ESTA ES UNA FUNCION QUE EMITE VALORES CUANDO EL ESTADO CAMBIA */
-
-    }, [])
-
+    const { status } = useCheckAuth()
 
 
 
